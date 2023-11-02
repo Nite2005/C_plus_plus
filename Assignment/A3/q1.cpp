3ca340b63f5dc8ab3343c5386f928aa3001c2e12
Nitesh Kumar <133790439+Nite2005@users.noreply.github.com>
2023-11-03 01:36:22 +0530

    Update q1.cpp

--- Assignment/A3/q1.cpp
+++ Assignment/A3/q1.cpp
@@ -1,1 +1,28 @@
+/*Q1 - Write a program to check whether two numbers (entered by user) are equal or not. (Easy)
+Sample Input 1: 1, 2
+Sample Output 1: 0
 
+Sample Input 2: 2, 2
+Sample Output 2: 1*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int x,y;
+    cout<<"Enter first number: ";
+    cin>>x;
+    cout<<"Enter second number: ";
+    cin>>y;
+
+    if(x==y)
+    {
+       cout<< true;
+
+    }
+    else{
+        cout<<false;
+    }
+    return 0;
+}
