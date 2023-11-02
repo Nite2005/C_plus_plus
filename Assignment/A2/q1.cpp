50961ac6628d095d4b40ecd83b8d4e3f5a80eb4b
Nitesh Kumar <133790439+Nite2005@users.noreply.github.com>
2023-11-03 00:33:05 +0530

    Update q1.cpp

--- Assignment/A2/q1.cpp
+++ Assignment/A2/q1.cpp
@@ -1,1 +1,18 @@
+/*Q1 - Take 2 integer values in two variables x and y and print their product. (Easy)
+Sample Input : x=2, y=4 (Both integers)
+Sample Output : 8 */
 
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int x,y;
+    cout<<"Enter first Number: ";
+    cin>>x;
+    cout<<"Enter second Number: ";
+    cin>>y;
+
+    cout<<"Multiplication of two numbers is : "<<x*y<<endl;
+    return 0;
+}
